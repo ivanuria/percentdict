75e12489eee664011cb4c6fc20e1414215dbc9a4
ivanuria <ivan.uria@gmail.com>
2021-07-30 18:36:46 +0200

    Plotted PercentDict

---
+++ __init__.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+from collections.abc import MutableMapping
+
+class PercentDict(MutableMapping):
+    def __init__(self, instance=None):
+        pass
+
+    def __contains__(self, item):
+        pass
+
+    def __delitem__(self, key):
+        pass
+
+    def __eq__(self, instance):
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __ne__(self, instance):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+    
+    def clear(self):
+        pass
+
+    def get(self):
+        pass
+
+    def items(self):
+        pass
+
+    def keys(self):
+        pass
+
+    def pop(self):
+        raise NotImplemented()
+
+    def popitem(self):
+        raise NotImplemented()
+
+    def setdefault(self, default):
+        pass
+
+    def update(self, instance):
+        pass
+    
+    def values(self):
+        pass
