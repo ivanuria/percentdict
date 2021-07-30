78219ac05c57a80a77c7c76633781779c01deddf
ivanuria <ivan.uria@gmail.com>
2021-07-30 18:48:39 +0200

    Plotted views

--- __init__.py
+++ __init__.py
@@ -1,10 +1,59 @@
 #!/usr/bin/env python3
 
-from collections.abc import MutableMapping
+from collections.abc import MutableMapping, KeysView, ValuesView, ItemsView
+
+class PercentKeys(KeysView):
+    def __init__(self, percentdict):
+        self._percentdict = percentdict
+
+    def __contains__(self, item):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def len(self):
+        pass
+
+    def reversed(self):
+        pass
+
+class PercentValues(ValuesView):
+    def __init__(self, percentdict):
+        self._percentdict = percentdict
+
+    def __contains__(self, item):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def len(self):
+        pass
+
+    def reversed(self):
+        pass
+
+class PercentItems(ValuesView):
+    def __init__(self, percentdict):
+        self._percentdict = percentdict
+
+    def __contains__(self, item):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def len(self):
+        pass
+
+    def reversed(self):
+        pass 
 
 class PercentDict(MutableMapping):
     def __init__(self, instance=None):
-        pass
+        self._keys = []
+        self._values = []
 
     def __contains__(self, item):
         pass
