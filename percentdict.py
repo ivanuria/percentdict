9533bb4c512c8b593cd5a8c47126d7ff65f52079
ivanuria <ivan.uria@gmail.com>
2021-07-31 18:04:39 +0200

    Added documentation header and fixed a bug in get with a slicce

--- percentdict.py
+++ percentdict.py
@@ -1,5 +1,73 @@
 #!/usr/bin/env python3
 
+"""
+PercentDict offers a dict like object that gives you a way to implement a selector between two values. This value is a float between 0 and 1. 
+
+To instantiate is so simple as:
+
+pd = PercentDict()
+
+Given the typical RPG table for throwing a d10. Step is not used in slices here:
+
+1 -> You PC is poor
+2-3 -> Your PC can survive
+4-7 -> Your PC is middle class
+8-9 -> Your PC is rich
+10 -> Your PC is millionare
+
+You can give the data this way:
+
+pd[0:0.1] = "You PC is poor"
+pd[0.1:0.3] = "Your PC can survive"
+pd[0.3:0.7] = "Your PC is middle class"
+pd[0.7:0.9] = "Your PC is rich"
+pd[1] = "Your PC is millionaire"
+
+The inferior limit will be used as superior limit of the previous one, so:
+
+pd[0.1]
+"You PC is poor"
+
+pd[0.15]
+"Your PC can survive"
+
+You can fill those beginning only setting the max value:
+
+pd[0.1] = "You PC is poor"
+pd[0.3] = "Your PC can survive"
+pd[0.7] = "Your PC is middle class"
+pd[0.9] = "Your PC is rich"
+pd[1] = "Your PC is millionaire"
+
+
+I.E:
+
+pd[0.15]
+"Your PC can survive"
+
+In case you'll use random library and you want to be sure the usage is similar to throwing a d10 like the example, use random.randint and divide it by the number of faces of the die. 10 in case of the example.
+
+I.E:
+pd[random.randint(10)/10]
+
+You can use other dict methods, like keys(), values()... But pop, popitem and setdefault are not implemented.
+
+For a more intuitive implementation in case you want to use like the example a dice like, you can use RangedDict. It will only accept integers in getters, setters and slices.
+
+I.E:
+
+rd = RangedDict(d=12) # d means the faces of the dice
+
+rd[1] = "You PC is poor"
+rd[2:4] = "Your PC can survive"
+rd[5:9] = "Your PC is middle class"
+rd[10:11] = "Your PC is rich"
+rd[12] = "Your PC is millionaire"
+
+pd[6]
+"Your PC is middle class"
+"""
+
 from collections.abc import MutableMapping
 from collections.abc import KeysView, ValuesView, ItemsView
 from collections.abc import Iterable
@@ -111,7 +179,6 @@ class PercentDict(MutableMapping):
             if key < 0 or 1 < key:
                 raise KeyError("Key must be a float between 0 and 1")
             for item in self._mapping:
-                print(f"{(key, item)}")
                 if key <= item[0]:
                     return item[1]
             else:
@@ -119,11 +186,14 @@ class PercentDict(MutableMapping):
             
         elif isinstance(key, slice):
             final = []
-            for item in reversed(self._mapping):
-                if key.stop <= item[0] or key.start <= item[0]:
+            for item in self._mapping:
+                if not final and key.start <= item[0]:
                     final.append(item[1])
-                elif key.start > item[0]:
-                    break
+                elif final:
+                    final.append(item[1])
+                    if key.stop <= item[0]:
+                        break
+
             if final:
                 return tuple(final)
             else:
